4919287d9ba5aaf6afc917199049e5c91d27ffc2
Jordyn Fulbright <jordynfulbright@gmail.com>
2022-05-06 18:25:58 -0400

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
+
+
+import { NFTData } from "./dummy";
+
+import assets from "./assets";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
